6bbe92e8d7922896b8dda1d6424986eca32f5617
Hung <hunghx@rikkeisoft.com>
2023-02-13 16:49:08 +0700

    fhud

---
+++ script.js
@@ -0,0 +1,7 @@
+const heart = document.querySelector(".heart");
+const img = document.querySelector(".img");
+const btn = document.querySelector(".btn");
+document.addEventListener("click", function () {
+  heart.style.display = "block";
+  img.style.display = "none";
+});
